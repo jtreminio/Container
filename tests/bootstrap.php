4b95d96d988868e26edd9f7743e8b64cae4b3833
Juan Treminio <jtreminio@gmail.com>
2013-04-07 13:38:29 -0500

    Renaming project

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use jtreminio\Container\Container;
-
-$loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->add('jtreminio\Container\Tests', __DIR__);
-
-Container::setPimple(new \Pimple);
